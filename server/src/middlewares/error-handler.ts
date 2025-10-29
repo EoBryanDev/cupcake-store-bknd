de9898794806c87045282c1519f7cf6ff54b11a8
Bryan-Galaxy-WSL-Ubuntu <eobryandev@gmail.com>
2025-10-29 13:23:22 -0300

    chore: adding db error in error-handler

--- server/src/middlewares/error-handler.ts
+++ server/src/middlewares/error-handler.ts
@@ -1,6 +1,7 @@
 import { Request, Response, NextFunction } from "express";
 import { AppError } from "../errors/AppError";
 import { ZodError } from "zod";
+import { IPostgresError } from "../interfaces/IPostgresError";
 
 // Middleware para capturar erros
 export function errorHandler(
@@ -12,10 +13,32 @@ export function errorHandler(
   if (err instanceof ZodError) {
     return res.status(400).json({
       status: "error",
-      message: "Erro de validação",
+      message: "Validation error",
       errors: err.flatten().fieldErrors,
     });
   }
+
+  const dbError = err.cause as IPostgresError;
+  if (dbError) {
+    switch (dbError.code) {
+      case "23505":
+        return res.status(409).json({
+          status: "error",
+          message: "Duplicated Register",
+        });
+      case "23502":
+        return res.status(400).json({
+          status: "error",
+          message: "Mandatory field is missing",
+        });
+      case "23503":
+        return res.status(400).json({
+          status: "error",
+          message: "Invalid reference",
+        });
+    }
+  }
+
   if (err instanceof AppError) {
     return res.status(err.statusCode).json({
       status: "error",
@@ -25,6 +48,6 @@ export function errorHandler(
 
   return res.status(500).json({
     status: "error",
-    message: "Erro interno do servidor",
+    message: "Internal Server Error",
   });
 }
